393531d682e65789e15ebb12b75d6f91fce4412e
Marcrobito <marco.martinez@pakenergy.com>
2023-04-22 08:45:13 -0600

    StartingPoint

---
+++ TestIos/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Marco Antonio Martinez Gutierrez on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
